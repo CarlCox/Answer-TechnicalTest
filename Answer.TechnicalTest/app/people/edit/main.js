a4f480645d29bf086610ba53fbde92c5547d226c
Carl Cox <carl.cox@answerconsulting.com>
2015-08-24 12:10:20 +0100

    implement edit screen cancel button

--- Answer.TechnicalTest/app/people/edit/main.js
+++ Answer.TechnicalTest/app/people/edit/main.js
@@ -7,7 +7,13 @@
         'use strict';
 
         return Marionette.ItemView.extend({
-            template: personview
-            
+            template: personview,
+            events: {
+                'click [data-backbone]#cancel': 'cancel',
+            },
+            cancel: function (e) {
+                e.preventDefault();
+                Backbone.history.navigate('', { trigger: true });
+            }
         });
     });
