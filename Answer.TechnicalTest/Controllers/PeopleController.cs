e5c366d9c69a38593d55f6bf55a170cd1b3222e9
Carl Cox <carl.cox@answerconsulting.com>
2015-08-24 10:43:26 +0100

    Implement api people list

--- Answer.TechnicalTest/Controllers/PeopleController.cs
+++ Answer.TechnicalTest/Controllers/PeopleController.cs
@@ -1,10 +1,13 @@
 ﻿namespace Answer.TechnicalTest.Controllers
 {
+    using System.Linq;
     using System.Net;
     using System.Web.Http;
 
     using TechTest.Data.Entities;
     using TechTest.Data.Repositories;
+
+    using Models;
 
     public class PeopleController : ApiController
     {
@@ -19,7 +22,31 @@
 
         public IHttpActionResult Get()
         {
-            return Ok();
+            return
+               Ok(
+                   _personRepository.GetAll()
+                       .Select(
+                           p =>
+                           new Models.Person
+                           {
+                               FirstName = p.FirstName,
+                               Id = p.PersonId,
+                               IsAuthorised = p.IsAuthorised,
+                               IsEnabled = p.IsEnabled,
+                               IsValid = p.IsValid,
+                               LastName = p.LastName,
+                               Colours =
+                                   p.Colours.Select(
+                                       c =>
+                                       new Models.Colour
+                                       {
+                                           Id = c.ColourId,
+                                           IsEnabled = c.IsEnabled,
+                                           IsSelected = true,
+                                           Name = c.Name
+                                       }).ToList()
+                           })
+                       .ToList());
         }
 
         public IHttpActionResult Get(int id)
@@ -27,7 +54,7 @@
             return Ok();
         }
 
-        public IHttpActionResult Put(Person model)
+        public IHttpActionResult Put(Models.Person model)
         {
             return StatusCode(HttpStatusCode.NoContent);
         }
