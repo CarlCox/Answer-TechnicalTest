b8ce218f01d241fd48c2e71740cc4c21c2118da5
Carl Cox <carl.cox@answerconsulting.com>
2015-08-24 10:40:00 +0100

    Add viewmodel functions to support palindrome fullname and Colour text

--- Answer.TechnicalTest/Models/Person.cs
+++ Answer.TechnicalTest/Models/Person.cs
@@ -1,6 +1,7 @@
 ﻿namespace Answer.TechnicalTest.Models
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     public class Person
     {
@@ -20,6 +21,31 @@
         public bool IsAuthorised { get; set; }
 
         public bool IsEnabled { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0} {1}", FirstName, LastName);
+            }
+        }
+
+        public bool IsPalindrome
+        {
+            get
+            {
+                var name = FirstName.ToLower() + LastName.ToLower();
+                return name == new string(name.Reverse().ToArray());
+            }
+        }
+
+        public string ColourText
+        {
+            get
+            {
+                return string.Join(",", Colours.Where(c => c.IsSelected).Select(x => x.Name).ToList());
+            }
+        }
 
         public ICollection<Colour> Colours { get; set; }
     }
