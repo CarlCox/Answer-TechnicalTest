a9d66c5b09732ab9fa38fb602c0461a81ee1d92e
Carl Cox <carl.cox@answerconsulting.com>
2015-08-24 10:35:52 +0100

    Added view models

---
+++ Answer.TechnicalTest/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿namespace Answer.TechnicalTest.Models
+{
+    using System.Collections.Generic;
+
+    public class Person
+    {
+        public Person()
+        {
+            Colours = new List<Colour>();
+        }
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool IsValid { get; set; }
+
+        public bool IsAuthorised { get; set; }
+
+        public bool IsEnabled { get; set; }
+
+        public ICollection<Colour> Colours { get; set; }
+    }
+}
